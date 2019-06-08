137f298fce5c4e314e74c09f691d546d37ac9dbf
manuel chantada <manuelchantada@hotmail.com>
2019-06-08 13:44:39 -0300

    Added Routing

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product_list.component.css']
 })
